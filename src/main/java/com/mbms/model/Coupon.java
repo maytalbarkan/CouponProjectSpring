5bbd7e41999a6ac36623318fa35d829aa93e7a52
maytalbarkan <km856974>
2019-07-10 14:20:38 +0300

    update

--- src/main/java/com/mbms/model/Coupon.java
+++ src/main/java/com/mbms/model/Coupon.java
@@ -1,9 +1,9 @@
 package com.mbms.model;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,17 +11,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
-import javax.validation.constraints.Future;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.URL;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import example1.model.Company;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -35,20 +25,39 @@ public class Coupon {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-
+	
+	@Basic(optional = false)
+	@Column(nullable = false)
 	private String title;
+	
+	@Basic(optional = false)
+	@Column(nullable = false)
 	private Date startDate;
 	
+	@Basic(optional = false)
+	@Column(nullable = false)
 	private Date endDate;
 
+	@Basic(optional = false)
+	@Column(nullable = false)
 	private int amount;
+	
+	@Basic(optional = false)
+	@Column(nullable = false)
 	private CouponCaregory category;
 
+	@Basic(optional = false)
+	@Column(nullable = false)
 	private String message;
-	@Positive(message = "The coupon price must be positive")
+
+	@Basic(optional = false)
+	@Column(nullable = false)
 	private double price;
-	@URL(message = "The image must be a URL")
+
+	@Basic(optional = false)
+	@Column(nullable = false)
 	private String image;
+	
 	@ManyToOne
 	@JsonIgnore
 	@Valid
